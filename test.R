38dbff6a03d257cbc81e58ca93049f269e106d52
lawsonb <lawsonb@users.noreply.github.com>
2015-03-14 12:36:22 -0700

    150314 12:36pm

---
+++ test.R
@@ -0,0 +1,14 @@
+
+library(shiny)
+library(shinyapps)
+
+# attempt to run app from github
+runGitHub("Consistency", "lawsonb", subdir="Version_3")
+
+
+runApp("Version_3")
+
+
+
+shinyapps::deployApp("Cauchynormal")
+
